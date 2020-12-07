3b6aa688b04d83a2779ff05b592cf285e4aa80ff
Josewd <63750044+Josewd@users.noreply.github.com>
2020-12-07 16:46:55 -0300

    esqueleto projeto feito

---
+++ src/Hooks/useAuthetication.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import { useHistory} from 'react-router-dom'
+
+
+
+export default function useAuthorization(){
+
+
+    const history = useHistory()
+
+    useEffect(()=>{
+
+        const token = localStorage.getItem('token')
+
+        !token && history.push('/')
+    }, [history])
+
+}
---
+++ src/Services/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+
+
+export const api = axios.create({
+    baseURL: 'https://us-central1-missao-newton.cloudfunctions.net/futureEatsA/'
+})
+
+
+
+
