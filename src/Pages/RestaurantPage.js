ffdc03caebc99ef98b564b3f4adc145d8cb65567
Josewd <63750044+Josewd@users.noreply.github.com>
2020-12-09 12:40:44 -0300

    primeira renderizacao do feed funcionando

--- src/Pages/RestaurantPage.js
+++ src/Pages/RestaurantPage.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { useParams } from 'react-router-dom';
+import GlobalStateContext from '../Global/GlobalStateContext';
 import useAuthorization from '../Hooks/useAuthetication';
 import useRequestData from '../Hooks/useRequestData';
 
 export default function RestaurantPage() {
-  //parametros passados da FeedPage
+  useAuthorization()
+
+
+  const {restaurantList} = useContext(GlobalStateContext)
   const params = useParams()
   //requisição direta no state
   const [restaurantData] = useRequestData(`/restaurants/${params.id}`)
-  useAuthorization()
+
+  const restaurant = restaurantList.filter(rest=>{
+    return rest.id === params.id
+  })
    
    
 
   return (<div>
-      {/* console de monitoramento dedados */}
-      {console.log(restaurantData && restaurantData.restaurant.products)}
-      {/* mapeamento dos dados com condição */}
+   
+      <div>
+          
+          <img src={restaurant[0] && restaurant[0].logoUrl} alt=""/>
+      </div>
+
       {restaurantData &&  restaurantData.restaurant.products.map((food)=> {
           return (<div key={food.id}>
             <h1>Categoria: {food.category}</h1>
-            <p>Nome do restaurante: {food.name}</p>
+            <p>Nome : {food.name}</p>
             <p>Descrição: {food.description}</p>
             <img  width="200px" src={food.photoUrl}/>
             <p>R$ {food.price.toFixed(2)}</p>
--- src/Pages/Styles/styles.js
+++ src/Pages/Styles/styles.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import TextField from '@material-ui/core/TextField';
 
 export const MainContainer = styled.div`
 
@@ -9,6 +8,56 @@ export const MainContainer = styled.div`
     flex-direction: column;
     height: 100vh;
     width: 100vw;
+    position: relative;
+`
+
+export const FormContainer = styled.form`
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: max-content;
+    width: 100%;
+`
+export const SearchContainer = styled.div`
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 85%;
+    position: absolute;
+    top:142px;
+    left: 70px;
+    margin-top: 0px;
+    margin-bottom: 0px;
+    border-radius: 2px;
+    border: solid 1px grey;
+    font-size: 46px;
+    padding-left: 20px;
+    
+`
+export const CategoryContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top:262px;
+    left: 10px;
+    overflow: scroll;
+    width: 95%;
+`
+export const RenderContainer = styled.div`
+
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: flex-start;
+    overflow: scroll ;
+    width: 100%;
+    height: 70vh;
+    position: absolute;
+    top:372px;
 
 `
 export const LogoInvert = styled.img`
@@ -18,6 +67,7 @@ export const LogoInvert = styled.img`
     margin: 16px;
 
 `
+
 export const Button = styled.button`
 
     width: 80%;
@@ -31,13 +81,25 @@ export const Button = styled.button`
     cursor: pointer;
     border-radius: 5px;
 `
+export const SearchInput = styled.input`
 
-export const FormContainer = styled.form`
+    width: 90%;
+    height: 56px;
+    padding: 16px 16px 16px 17px;
+    border: none;
+    outline: none;
+    font-size: 35px;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    height: max-content;
-    width: 100%;
+`
+export const CategoryItem = styled.li`
+
+    list-style: none;
+    margin: 50px 50px;
+    font-size: 30px;
+    letter-spacing: 0.36;
+    &:hover{
+        color: #5cb646;
+    }
+
+
 `
