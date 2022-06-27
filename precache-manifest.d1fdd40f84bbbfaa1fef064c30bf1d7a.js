dd5b2916b21766d4e30174ce48a21279ee932d0b
Índio Medeiros <bboyindio@gmail.com>
2022-06-27 15:39:32 -0300

    Updates

---
+++ precache-manifest.d1fdd40f84bbbfaa1fef064c30bf1d7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84963a2ebf109508cb9b2e0d9e7d41d1",
+    "url": "/futureEats-front-end/index.html"
+  },
+  {
+    "revision": "e1fe930a073222d38df3",
+    "url": "/futureEats-front-end/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "2aeb68d5a5c4942876c2",
+    "url": "/futureEats-front-end/static/js/2.4fe9247b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/futureEats-front-end/static/js/2.4fe9247b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1fe930a073222d38df3",
+    "url": "/futureEats-front-end/static/js/main.100fa785.chunk.js"
+  },
+  {
+    "revision": "5691202432e78b11783b",
+    "url": "/futureEats-front-end/static/js/runtime-main.da249008.js"
+  },
+  {
+    "revision": "6cda6996d2909f08d1080e82d35a171f",
+    "url": "/futureEats-front-end/static/media/logo-future-eats@3x.6cda6996.png"
+  }
+]);
